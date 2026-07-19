3ed7cb43a8e5ff64579f599a01c0b2beb4a4452e
miquel5 <llauradofilella@gmail.com>
2026-07-19 15:33:46 +0200

    If the trip is only one day long, enter only one date

--- frontend/scripts/DataScripts.ts
+++ frontend/scripts/DataScripts.ts
@@ -1,7 +1,8 @@
 export function formatTravelDates (start: string, end: string) {
   const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'long' }
+
   const startStr = new Date(start).toLocaleDateString('es-ES', options)
   const endStr = new Date(end).toLocaleDateString('es-ES', options)
 
-  return `${startStr} a ${endStr}`
+  return startStr === endStr ? `${startStr}` : `${startStr} a ${endStr}`
 }
