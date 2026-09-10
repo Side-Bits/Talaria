54a91fb1be63c693cea234bd8df38ba53c06636b
miquel5 <llauradofilella@gmail.com>
2026-09-10 22:58:24 +0200

    Refactor

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -15,13 +15,28 @@ export function Header({ code, label }: Props) {
     <ThemedView type='between' style={styles.header}>
       {(code == "002" || code == "003" || code == "004" || code == "005") && (
         <Pressable onPress={() => router.back()} >
-          <Ionicons name="arrow-back-outline" size={20} color={Colors.light.onSurface} />
+          <Ionicons name="chevron-back-outline" size={20} color={Colors.light.textMuted} />
         </Pressable>
       )}
       <ThemedText type="title">{label}</ThemedText>
-      {(code == "ZZZ") ?
-        <Ionicons name="menu-outline" size={20} color={Colors.light.onSurface} onPress={() => console.log('menu-outline')} />
-      : <View/>}
+      {/*code === "ZZZ" ? (
+        <Ionicons
+          name="menu-outline"
+          size={20}
+          color={Colors.light.onSurface}
+          onPress={() => console.log("menu-outline")}
+        />
+      ) :*/ code === "001" ? (
+        <Ionicons
+          name="menu-outline"
+          size={20}
+          style={styles.icon}
+          color={Colors.light.onSurface}
+          onPress={() => console.log("menu-outline")}
+        />
+      ) : (
+        <View />
+      )}
     </ThemedView>
   );
 }
@@ -30,5 +45,13 @@ const styles = StyleSheet.create({
   header: {
     paddingVertical: 32,
     alignItems: 'center',
+  },
+  icon: {
+    paddingVertical: 4,
+    paddingHorizontal: 5,
+    backgroundColor: Colors.light.onSecondary,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: Colors.light.border,
   }
 });
